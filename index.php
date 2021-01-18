bd61cdb24e7c73a7f4bae04a8772ef6f2e08dc36
davide sbalzer <sbalzerdavide@hotmail.it>
2021-01-18 17:24:18 +0100

    basic style

--- index.php
+++ index.php
@@ -10,25 +10,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
     
     <header>
-        <h1>prodotti</h1>
+        <h1>Prodotti</h1>
     </header>
 
     <main>
         <div class="product">
-        <div class="shoes">
+            <div class="shoes">
                 <h2>Scarpe</h2>
-                <h3>
+                <h3 class="name">
                     <?php 
-                    
                     $first_shoes = new Shoes('Nike', 70 , 42);
-                    // var_dump($first_shoes);
                     echo $first_shoes->name;
-                    
                     ?>
                 </h3>
                 <div class="discount">
@@ -43,20 +41,15 @@
                         Prezzo originale:
                         <?php echo $first_shoes->price ?>€ 
                         <br>
-
                         Prezzo scontato: 
                         <?php echo $first_shoes->makeDiscount(); ?>€
                     </h4>
-
-                        <?php 
-                        // var_dump($first_shoes); 
-                        ?>
                 </div>
             
             </div>
             <div class="jackets">
                 <h2>Jackets</h2>
-                <h3>
+                <h3 class="name">
                     <?php 
                     
                     $first_jacket = new Jackets('NorthFace', 210, 10);
@@ -78,33 +71,32 @@
                         <?php echo $first_jacket->price ?>€ 
                         <br>
                         Prezzo scontato: 
-                        <?php echo $first_jacket->makeDiscount(); ?>€
-
-                        
-                        
+                        <?php echo $first_jacket->makeDiscount(); ?>€                        
                     </h4>
 
                     <?php 
                     // var_dump($first_jacket); 
                     ?>
                 </div>
-                <div class="trousers">
-                    <h2>Trousers</h2>
-                    <h3>
-                        <?php
-                        $first_trousers = new Trousers('Levis', 130);
-                        echo $first_trousers->name;
-                        ?>
-                    </h3>
+            
+            </div>
+            <div class="trousers">
+                <h2>Trousers</h2>
+                <h3 class="name">
+                    <?php
+                    $first_trousers = new Trousers('Levis', 130);
+                    echo $first_trousers->name;
+                    ?>
+                </h3>
+                <div class="discount">
                     <h3>
                         <?php echo $first_trousers->getDiscount();?>
                     </h3>
                     <h4>
                     Prezzo originale:
                         <?php echo $first_trousers->price ?>€ 
-                    </h4>
+                    </h4>                
                 </div>
-            
             </div>
         </div>
     </main>
